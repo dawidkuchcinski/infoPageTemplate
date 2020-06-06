ead993f27cbb5492e901f2811f3b094468112971
dkuchcinski <dawid.kuchcinski@setget.pl>
2020-06-06 22:03:56 +0200

    Zmień skrypt deploya i wymagane pakiety

--- deployscript.sh
+++ deployscript.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install python3-pip apache2 libapache2-mod-wsgi-py3 git
+sudo apt-get install -y python3-pip apache2 libapache2-mod-wsgi-py3 git
 
-sudo pip install virtualenv
+sudo pip3 install virtualenv
 
 mkdir ~/fake_wix
 cd ~/fake_wix
 
-pip install -r requirements.txt
+git clone https://github.com/dawidkuchcinski/infoPageTemplate.git
 
+virtualenv fake_wixenv
+
+source fake_wixenv/bin/activate
+
+cd infoPageTemplate
+
+pip3 install -r requirements.txt
+
+python ./manage.py makemigrations
+python ./manage.py migrate
+
+python ./manage.py createsuperuser
