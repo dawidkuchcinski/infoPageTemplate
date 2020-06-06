1c69ad13a0e1a85c43a51fe0d613adb536125983
dkuchcinski <dawid.kuchcinski@setget.pl>
2020-06-06 22:11:04 +0200

    Podnieś wersję Django do 3.0.7

--- deployscript.sh
+++ deployscript.sh
@@ -18,7 +18,7 @@ cd infoPageTemplate
 
 pip3 install -r requirements.txt
 
-python ./manage.py makemigrations
-python ./manage.py migrate
+python3 ./manage.py makemigrations
+python3 ./manage.py migrate
 
-python ./manage.py createsuperuser
+python3 ./manage.py createsuperuser
