8d193adc96f6173c2aa4491194f9e55f869d2942
dkuchcinski <dawid.kuchcinski@setget.pl>
2020-06-09 19:08:47 +0200

    Zmiana modelu banerów

--- infopagetemplateapp/models.py
+++ infopagetemplateapp/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Banner(models.Model):
     ban_name = models.CharField(max_length=255, default='banne_name')
     ban_placeholder = models.CharField(max_length=3, default='pl1')
-    ban_img = models.ImageField(upload_to='static/img',max_length=100)
+    ban_img = models.ImageField(upload_to='',max_length=100)
 
     def __str__(self):
         return self.ban_name
